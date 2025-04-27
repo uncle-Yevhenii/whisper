eb4dd9a84db835324faf1737db62c5e4c95d5e86
yevhenii <yevhenii.katsimon@gmail.com>
2025-04-27 22:02:58 +0000

    chore: initialize project with TypeScript, ESLint, Prettier, and Husky configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+  },
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.node },
+  },
+  tseslint.configs.recommended,
+  eslintPluginPrettier,
+  {
+    rules: {
+      "no-console": ["error", { allow: ["warn", "error"] }],
+    },
+  },
+]);
