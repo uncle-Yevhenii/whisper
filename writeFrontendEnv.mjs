680232fcf048c06ca30857757074c169f80a948b
yevhenii <yevhenii.katsimon@gmail.com>
2025-05-07 13:14:44 +0000

    feat: update prebuild script to use writeFrontendEnv.mjs for environment variable setup

---
+++ writeFrontendEnv.mjs
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv';
+import { writeFileSync } from 'fs';
+
+dotenv.config();
+
+const content = `
+VITE_REACT_APP_BASE_URL=${process.env.VITE_REACT_APP_BASE_URL}
+VITE_APP_NAME=${process.env.VITE_APP_NAME}
+`;
+
+writeFileSync('./frontend/.env.local', content);
+console.warn('[writeFrontendEnv] .env.local written successfully');
